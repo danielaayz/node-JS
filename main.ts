ad169b8bf55ce63cd8cc80d1b1346e437e88eb12
Daniela Y. Zagal <149149177+danielaayz@users.noreply.github.com>
2024-09-23 15:34:45 +0200

    new function

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 import fs from "fs";
 import path from "path";
-import { reverseString, countCharacters } from "./stringUtils";
+import { reverseString, countCharacters, capitalizeWords } from "./stringUtils";
 
 // 1) Write a sentence about why learning Node.js is important
 const data =
@@ -11,6 +11,7 @@ const data =
 console.log(reverseString(data));
 // ii) Count the characters
 console.log(countCharacters(data));
+//console.log(capitalizeWords(data));
 // Writes the reversed sentence into a new file
 fs.writeFileSync("reversed.txt", reverseString(data));
 console.log("File written successfully");
--- stringUtils.ts
+++ stringUtils.ts
@@ -5,3 +5,12 @@ export const reverseString = (string: string): string =>
 // A function to count the characters in a sentence
 export const countCharacters = (string: string): number =>
    string.replace(/\s+/g, "").length;
+
+// A function which will capitalize the first letter of every word in a string
+export const capitalizeWords = (string: string): string =>
+   string
+      .split(" ")
+      .map(
+         (word) => word.charAt(0).toUpperCase() + word.substring(1, word.length)
+      )
+      .join(" ");
