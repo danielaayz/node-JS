3ca2cb6e6aa3f375c389a23f1e345ae1eb3fb9bb
Daniela Y. Zagal <149149177+danielaayz@users.noreply.github.com>
2024-09-23 13:11:52 +0200

    build in modules

--- main.ts
+++ main.ts
@@ -1,11 +1,18 @@
+import fs from "fs";
+import path from "path";
 import { reverseString, countCharacters } from "./stringUtils";
 
 // 1) Write a sentence about why learning Node.js is important
-const sentence =
+const data =
    "Node.js is crucial because it allows developers to build scalable, high-performance web applications using JavaScript for both server-side and client-side code, enabling real-time data handling and fast execution";
 
 // 2) Use your functions to:
 // i) Reverse the sentence
-console.log(reverseString(sentence));
+console.log(reverseString(data));
 // ii) Count the characters
-console.log(countCharacters(sentence));
+console.log(countCharacters(data));
+// Writes the reversed sentence into a new file
+fs.writeFileSync("reversed.txt", reverseString(data));
+console.log("File written successfully");
+const filePath: string = path.join(__dirname, "reversed.txt");
+console.log(filePath);
