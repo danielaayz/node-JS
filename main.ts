db17f9ffaee049f3c743783527a5c31619233ed5
Daniela Y. Zagal <149149177+danielaayz@users.noreply.github.com>
2024-09-23 12:37:07 +0200

    import module

---
+++ main.ts
@@ -0,0 +1,11 @@
+import { reverseString, countCharacters } from "./stringUtils";
+
+// 1) Write a sentence about why learning Node.js is important
+const sentence =
+   "Node.js is crucial because it allows developers to build scalable, high-performance web applications using JavaScript for both server-side and client-side code, enabling real-time data handling and fast execution";
+
+// 2) Use your functions to:
+// i) Reverse the sentence
+console.log(reverseString(sentence));
+// ii) Count the characters
+console.log(countCharacters(sentence));
