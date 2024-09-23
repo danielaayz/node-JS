17c36d7f8b649d7136499c18c061d43e2f07cd80
Daniela Y. Zagal <149149177+danielaayz@users.noreply.github.com>
2024-09-23 12:36:36 +0200

    new functions

---
+++ stringUtils.ts
@@ -0,0 +1,10 @@
+export const reverseString = (string: string): string => {
+   return string.split("").reverse().join("");
+};
+
+export const countCharacters = (string: string): number => {
+   return string.length;
+};
+
+// export const add = (a,b) => a + b;
+// export const subtract = (a,b) => a - b;
